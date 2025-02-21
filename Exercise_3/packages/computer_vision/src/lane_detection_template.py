4964940a7237517ba114c126650f0e938de85827
anthony <zhaoyu5@ualberta.ca>
2025-02-20 19:18:39 -0700

    Changes by zhaoyu4

--- Exercise_3/packages/computer_vision/src/lane_detection_template.py
+++ Exercise_3/packages/computer_vision/src/lane_detection_template.py
@@ -16,9 +16,16 @@ class LaneDetectionNode(DTROS):
     def __init__(self, node_name):
         super(LaneDetectionNode, self).__init__(node_name=node_name, node_type=NodeType.PERCEPTION)
         # add your code here
-        
+
         # camera calibration parameters (intrinsic matrix and distortion coefficients)
-        
+        self.camera_matrix = np.array([[729.3017308196419, 0.0, 296.9297699654982],
+                                       [0.0, 714.8576567892494, 194.88265037301576],
+                                       [0.0, 0.0, 1.0]])
+
+        # Distortion coefficients (example values, replace with actual ones)
+        self.dist_coeffs = np.array(
+            [[-1.526832375685591], [2.217300696985744], [-0.00035517449407590306], [-0.013740460640726298], [0.0]])
+
         # color detection parameters in HSV format
         self.yellow_lower = np.array([20, 100, 100])
         self.yellow_upper = np.array([30, 255, 255])
@@ -26,13 +33,20 @@ class LaneDetectionNode(DTROS):
         self.white_upper = np.array([255, 30, 255])
 
         # initialize bridge and subscribe to camera feed
+        self._vehicle_name = os.environ['VEHICLE_NAME']
+        self._camera_topic = f"/{self._vehicle_name}/camera_node/image/compressed"
+
+        self._bridge = CvBridge()
+        self.pub = rospy.Publisher(f"/{self._vehicle_name}/processed_image", Image, queue_size=1)
+        self.sub = rospy.Subscriber(self._camera_topic, CompressedImage, self.callback)
 
         # lane detection publishers
 
         # LED
-        
+
         # ROI vertices
-        
+        self.roi = None
+
         # define other variables as needed
         self.rate = rospy.Rate(5)
 
@@ -44,14 +58,19 @@ class LaneDetectionNode(DTROS):
         undistorted = cv2.undistort(image, self.camera_matrix, self.dist_coeffs, None, new_camera_matrix)
         return undistorted
 
-    def preprocess_image(self, **kwargs):
+    def preprocess_image(self, image):
         # add your code here
-        pass
-    
+        if self.roi is None:
+            return
+
+        x, y, w, h = self.roi
+        resized = image[y:y+h, x:x+w]
+        return cv2.GaussianBlur(resized, (5, 5), 0)
+
     def detect_lane_color(self, **kwargs):
         # add your code here
         pass
-    
+
     def detect_lane(self, **kwargs):
         # add your code here
         # potentially useful in question 2.1
@@ -59,25 +78,29 @@ class LaneDetectionNode(DTROS):
 
     def callback(self, msg):
         # add your code here
-        
+
         # convert compressed image to CV2
-        
+        image = self._bridge.compressed_imgmsg_to_cv2(msg)
+
         # undistort image
+        undistorted_image = self.undistort_image(image)
+        self.pub.publish(self._bridge.cv2_to_imgmsg(undistorted_image, encoding="bgr8"))
 
         # preprocess image
+        preprocessed_image = self.preprocess_image(undistorted_image)
 
-        # detect lanes - 2.1 
-        
+        # detect lanes - 2.1
+
         # publish lane detection results
-        
+
         # detect lanes and colors - 1.3
-        
+
         # publish undistorted image
-        
+
         # control LEDs based on detected colors
 
         # anything else you want to add here
-        
+        self.rate.sleep()
         pass
 
     # add other functions as needed
