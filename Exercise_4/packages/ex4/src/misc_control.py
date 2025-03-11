32a2fd428041de76a8565642ed63fbaa67727c6d
Sami Jagirdar <jagirdar@ualberta.ca>
2025-03-11 12:44:03 -0600

    added miscellaneous control service node

---
+++ Exercise_4/packages/ex4/src/misc_control.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+# import required libraries
+import os
+import rospy
+from duckietown.dtros import DTROS, NodeType
+from ex4.srv import MiscCtrlCMD, MiscCtrlCMDResponse
+from duckietown_msgs.msg import LEDPattern
+from std_msgs.msg import ColorRGBA
+
+
+class MiscellaneousControl(DTROS):
+    def __init__(self, node_name):
+        super(MiscellaneousControl, self).__init__(node_name=node_name, node_type=NodeType.CONTROL)
+        # add your code here
+        
+        # robot params
+        self._vehicle_name = os.environ['VEHICLE_NAME']
+
+        # Param address for the camera framerate
+        self._camera_framerate_param = f"/{self._vehicle_name}/camera_node/framerate"
+
+        # Save the original value to restore later 
+        self.original_framerate = rospy.get_param(self._camera_framerate_param)
+
+        # define other variables as needed
+        self.cmd = ["set_fr", "reset_fr", "set_led"]
+        self.colors = [ColorRGBA(r=1, g=0, b=0, a=0.5),  # red
+                       ColorRGBA(r=0, g=0, b=1, a=0.5),  # blue
+                       ColorRGBA(r=0, g=1, b=0, a=0.5),  # green
+                       ColorRGBA(r=1, g=1, b=1, a=0.5)]  # white
+
+        # Set shutdown callback to restore framerate to the original value
+        rospy.on_shutdown(self.on_shutdown)
+        
+    def set_framerate(self, fr):
+        rospy.set_param(self._camera_framerate_param, fr)
+        rospy.loginfo(f"Original camera framerate: {self.original_framerate}. Setting it to {fr}")
+
+    def reset_framerate(self):
+        rospy.set_param(self._camera_framerate_param, self.original_framerate)
+        rospy.loginfo(f"Reset framerate to {self.original_framerate}")
+
+    def callback(self, msg):
+        cmd, value = msg.cmd, msg.value
+
+        if cmd not in self.cmd:
+            return MiscCtrlCMDResponse(False, f"Invalid command. Accepted commands are: {self.cmd}.")
+        
+        if cmd == self.cmd[0]:
+            if not 0 < value <= 30:
+                return MiscCtrlCMDResponse(False, "Framerate must be between 0 and 31 exclusive.")
+            
+            self.set_framerate(value)
+            return MiscCtrlCMDResponse(True, f"Successfully set framerate to {value}")
+
+        elif cmd == self.cmd[1]:
+            self.reset_framerate()
+            return MiscCtrlCMDResponse(True, f"Successfully reset framerate to {self.original_framerate}")
+        
+    def on_shutdown(self):
+        self.reset_framerate()
+
+
+if __name__ == '__main__':
+    node = MiscellaneousControl(node_name='miscellaneous_control_node')
+    s = rospy.Service('misc_ctrl_srv', MiscCtrlCMD, node.callback)
+    rospy.spin()
